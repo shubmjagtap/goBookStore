256f01bbc2659b990179113b418f01886e780043
shubmjagtap <smjdevopservices@gmail.com>
2023-06-18 23:35:15 +0530

    added book-controller.go

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/shubmjagtap/goBookStore/pkg/utils"
+	"github.com/shubmjagtap/goBookStore/pkg/models"
+)
